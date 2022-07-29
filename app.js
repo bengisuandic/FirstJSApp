0b75903ed6108cecd42be4cb73eae2c1dc494225
bengisuandic <bengisuandic@sabanciuniv.edu>
2022-07-29 15:15:32 +0300

    Callback hell

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ document.getElementById("turn-yellow").onclick = yellowFunction;
 document.getElementById("turn-pink").onclick = pinkFunction;
 document.getElementById("turn-default").onclick = defFunction;
 
+//An example of a callback hell (a.k.a. christmas tree)
+document.getElementById("go").addEventListener("click", function (e) {
+  const el = e.currentTarget;
+  console.log(el);
+  setTimeout(function() {
+    el.classList.add("circle");
+    setTimeout(function() {
+      el.classList.add("red");
+      setTimeout(function() {
+        el.classList.remove("circle");
+        setTimeout(function() {
+          el.classList.remove("red");
+          el.classList.remove("purple");
+          setTimeout(function() {
+            el.classList.add('dontFadeOut');
+          })
+        }, 300);
+      }, 300);
+    }, 500);
+  }, 2000);
+});
 
 function defFunction() {
     document.getElementsByTagName("BODY")[0].style.backgroundColor = "white";
